ff98b0733196b49d46ff38af90007c063b786b0e
Taiki Endo <te316e89@gmail.com>
2025-09-13 00:29:45 +0900

    Fix typos

--- src/task.rs
+++ src/task.rs
@@ -237,7 +237,7 @@ impl<T, M> Task<T, M> {
             let mut output = None;
 
             // Optimistically assume the `Task` is being detached just after creating the task.
-            // This is a common case so if the `Task` is datached, the overhead of it is only one
+            // This is a common case so if the `Task` is detached, the overhead of it is only one
             // compare-exchange operation.
             if let Err(mut state) = (*header).state.compare_exchange_weak(
                 SCHEDULED | TASK | REFERENCE,
